f3ff7d61e80a0d7675e73a5db89151f57ddcabdf
Andy Whetstone <andy_whetstone@live.com>
2021-11-02 01:02:49 -0500

    Project Complete

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def index
+    flickr = Flickr.new
+    @photos = flickr.photos.getRecent(:api_key => ENV['FLICKR_API_KEY'], :per_page => 6)
+    if params[:flickr_id]
+      begin
+        @photos = flickr.people.getPublicPhotos(:user_id => params[:flickr_id], :api_key => ENV['FLICKR_API_KEY'])
+      rescue Flickr::FailedResponse
+        flash[:notice] = "That user does not exist"
+      end
+    end
+  end
+end
---
+++ config/initializers/figaro.rb
@@ -0,0 +1,1 @@
+Figaro.require_keys("FLICKR_APP_ID", "FLICKR_API_KEY", "FLICKR_SHARED_SECRET")
